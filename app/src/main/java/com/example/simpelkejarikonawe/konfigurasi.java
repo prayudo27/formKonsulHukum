fc4fa82c437ff6aec8b4d74b46f10d2b08bbe40d
prayudo <prayudo27@gmail.com>
2019-06-16 20:37:23 +0700

    first commit

---
+++ app/src/main/java/com/example/simpelkejarikonawe/konfigurasi.java
@@ -0,0 +1,33 @@
+package com.example.simpelkejarikonawe;
+
+public class konfigurasi {
+    public static final String URL_ADD="http://192.168.43.190/konsul_hukum/create.php";
+    public static final String URL_READ_DATA_ALL="http://192.168.43.190/konsul_hukum/read_data_all.php";
+    public static final String URL_READ_DATA="http://192.168.43.190/konsul_hukum/read_data.php?id=";
+    public static final String URL_UPDATE="http://192.168.43.190/konsul_hukum/update.php";
+    public static final String URL_DELETE="http://192.168.43.190/konsul_hukum/delete.php?id=";
+
+    public static final String KEY_ID_KONSUL= "id";
+    public static final String KEY_STASIUN_RADIO = "stasiun_radio";
+    public static final String KEY_NAMA = "nama";
+    public static final String KEY_JABATAN = "jabatan";
+    public static final String KEY_ALAMAT = "alamat";
+    public static final String KEY_TELEPON = "telepon";
+    public static final String KEY_EMAIL = "email";
+    public static final String KEY_MATERI = "materi";
+    public static final String KEY_WAKTU_PELAKSANAAN = "waktu_pelaksanaan";
+
+    public static final String TAG_JSON_ARRAY="result";
+    public static final String TAG_ID_KONSUL="id";
+    public static final String TAG_STASIUN_RADIO = "stasiun_radio";
+    public static final String TAG_NAMA="nama";
+    public static final String TAG_JABATAN="jabatan";
+    public static final String TAG_ALAMAT="alamat";
+    public static final String TAG_TELEPON="telepon";
+    public static final String TAG_EMAIL="email";
+    public static final String TAG_MATERI="materi";
+    public static final String TAG_WAKTU_PELAKSANAAN="waktu_pelaksanaan";
+
+    public static final String ID_KONSUL = "id_konsul";
+
+}
